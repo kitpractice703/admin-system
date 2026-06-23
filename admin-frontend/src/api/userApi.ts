0dce7efe623f8062a2a3e58174fc6acd6eca6ef5
intaekim <kitpractice703@gmail.com>
2026-06-23 12:47:22 +0900

    feat: admin-frontend/api 작성

---
+++ admin-frontend/src/api/userApi.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:8082";
+
+export interface UserResponse {
+  id: number;
+  username: string;
+  email: string;
+  phoneNumber: string;
+  createdAt: string;
+}
+
+export interface UserRequest {
+  username: string;
+  email: string;
+  phoneNumber: string;
+}
+
+export const getUsers = () => axios.get<UserResponse[]>(`${BASE_URL}/users`);
+
+export const getUserById = (id: number) =>
+  axios.get<UserResponse>(`${BASE_URL}/users/${id}`);
+
+export const createUser = (data: UserRequest) =>
+  axios.post<UserResponse>(`${BASE_URL}/users`, data);
+
+export const updateUser = (id: number, data: UserRequest) =>
+  axios.put<UserResponse>(`${BASE_URL}/users/${id}`, data);
+
+export const deleteUser = (id: number) =>
+  axios.delete(`${BASE_URL}/users/${id}`);
